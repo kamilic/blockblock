d3dda076e2d31cdeaba771c4fc258b87d4e4ccc8
wangjin <wangjin@mama.cn>
2019-08-23 17:09:29 +0800

    feat: first commit

--- README.md
+++ README.md
@@ -1,25 +1,46 @@
 # blockblock
 Representing values use chars.
 
-Usage
+## Usage
 ### default
 ```javascript
 const BB = require('blockblock');
 const b = BB();
 b.value(10); // ██████████
 b.value(10.1); // ██████████
-b.value(10.126) // ██████████
+b.value(10.126); // ██████████
 b.value(10.25); // ██████████▎
-b.percentage(10) // ██████████
+
+b.percentage(10); // ██████████
 ```
 
 ### custom emoji blockblock
 ```javascript
 const BB = require('blockblock');
-const b = BB();
-b.value(10); // ██████████
-b.value(10.1); // ██████████
-b.value(10.126) // ██████████
-b.value(10.25); // ██████████▎
-b.percentage(10) // ██████████
+const b = BB({
+    max: 12,
+    stepBlocks: ["😞", "😔", "😟", "☹️", "😑", "😐", "😏", "🙂", "😀", "😆"]
+});
+b.value(10); // "😆😆😆😆😆😆😆😆😆😆"
+b.value(10.125); // "😆😆😆😆😆😆😆😆😆😆😞"
+b.value(10.2); // "😆😆😆😆😆😆😆😆😆😆😔"
+
+b.precentage(100) // "😆😆😆😆😆😆😆😆😆😆😆😆"
 ```
+
+## Options
+```javascript
+const F = require('blockblock');
+const bb = BB({
+    // Maximum value
+    max: 100,
+    // Define different chars for represeting value (0 < value <= 1)
+    // "▏", "▎", "▍", "▌", "▋", "▊", "▉", "█"
+    // 1/8, 2/8, 3/8, 4/8, 5/8, 6/8, 7/8, 8/8
+    stepBlocks: ["▏", "▎", "▍", "▌", "▋", "▊", "▉", "█"]
+});
+```
+
+## Methods
+- `bb.value(value: number): string` - get blockblock string.
+- `bb.percentage(percent: number): string` - get blockblock string using percentage.(Based on provided `max`.
--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@
  * @example
  * "▏", "▎", "▍", "▌", "▋", "▊", "▉", "█"
  * 1/8, 2/8, 3/8, 4/8, 5/8, 6/8, 7/8, 1
- * 
+ *
  * 0 < value <= 1/8 => "▏"
  * 1/8 < value <= 2/8 => "▎"
  * ...
  * 7/8 < value <= 1 => "▎"
- * 
+ *
  */
 type BBSettings = {
   max: number;
@@ -19,26 +19,26 @@ type BBSettings = {
 };
 
 interface BB {
-  value(v: number): string,
-  percentage(p: number): string
+  value(v: number): string;
+  percentage(p: number): string;
 }
 
 const conf: BBSettings = {
   max: 100,
-  stepBlocks: ["▏", "▎", "▍", "▌", "▋", "▊", "▉", "█"]
+  stepBlocks: ['▏', '▎', '▍', '▌', '▋', '▊', '▉', '█'],
 };
 
 function getDecimalPart(value: number): number {
-  const part = value.toString().split(".")[1];
+  const part = value.toString().split('.')[1];
   return part ? parseFloat(`0.${part}`) : 0;
 }
 
 function valueResolver(value: number, settings: BBSettings): string {
-  const { stepBlocks} = settings;
+  const { stepBlocks } = settings;
   const fullSteps = Math.floor(value);
   const fracStep = getDecimalPart(value);
   const lengthOfStepBlocks = stepBlocks.length;
-  const onestepChar = stepBlocks[lengthOfStepBlocks - 1] || "";
+  const onestepChar = stepBlocks[lengthOfStepBlocks - 1] || '';
 
   return Array(fullSteps)
     .fill(0)
@@ -46,28 +46,29 @@ function valueResolver(value: number, settings: BBSettings): string {
     .concat(
       fracStep > 1 / lengthOfStepBlocks
         ? stepBlocks.reduce((pre, value, index) => {
-            return (index + 1) / lengthOfStepBlocks <= fracStep ? value : pre;
-          })
-        : [""]
+          return (index + 1) / lengthOfStepBlocks <= fracStep ? value : pre;
+        })
+        : ['']
     )
-    .join("");
+    .join('');
 }
 
 /**
- * @param {BBSetting} opt 
+ * @description Factory
+ * @param {BBSetting} opt
  */
 function f(opt: BBSettings): BB {
   const settings = Object.assign({}, conf, opt);
 
   return {
-    value(value: number) {
+    value(value: number): string {
       return valueResolver(value, settings);
     },
 
-    percentage(percent: number) {
+    percentage(percent: number): string {
       const { max } = settings;
       return valueResolver((max * percent) / 100, settings);
-    }
+    },
   };
 }
 
